d97f02d35ff4a1a63be3ff4d6d3c2af8ebccb79c
Sarkorbek Abdullaev <sarkorbek7777@gmail.com>
2023-01-16 21:54:11 +0500

    starter file

---
+++ src/components/card/card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="card__images">
+        <img src="" alt="" className="card__images--img" />
+      </div>
+      <p>June 29th, 2021</p>
+      <h3>The 2020 World's Most Valuable Brands</h3>
+      <p>
+        Non sed molestie tortor massa vitae in mattis. Eget vel consequat
+        hendrerit commodo libero aliquam. Urna arcu nunc tortor vitae
+        pharetra...
+      </p>
+      <button>Read more</button>
+    </div>
+  );
+}
+
+export default Card;
